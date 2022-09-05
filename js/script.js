b382811470d2c4a3b6061d6f3480a093a5c9c7bc
Jahi Grant <jahigrant@googlemail.com>
2022-09-05 13:27:09 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-console.log('hello mate')
+
+fetch('https://www.freecodecamp.dev/curriculum-data/v1/available-superblocks.json')
+.then((response) => response.json())
+.then((data) => console.log(data));

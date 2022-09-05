48719f24cc32c1dc6669aa8cacb65753deeae600
Jahi Grant <jahigrant@googlemail.com>
2022-09-05 14:03:04 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 
-fetch('https://www.freecodecamp.dev/curriculum-data/v1/available-superblocks.json')
-.then((response) => response.html())
-.then((data) => console.log(data));
 
+
+fetch('https://www.freecodecamp.dev/curriculum-data/v1/available-superblocks.json').then(function (response) {
+	// The API call was successful!
+	return response.text();
+}).then(function (html) {
+
+	// Convert the HTML string into a document object
+	var parser = new DOMParser();
+	var doc = parser.parseFromString(html, 'text/html');
+
+}).catch(function (err) {
+	// There was an error
+	console.warn('Something went wrong.', err);
+});

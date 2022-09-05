6c4c034c5cdca264c4a72d1f88601f35a4032097
Jahi Grant <jahigrant@googlemail.com>
2022-09-05 13:41:37 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
 
-fetch('https://www.freecodecamp.dev/curriculum-data/v1/available-superblocks.json')
-.then((response) => response.json())
-.then((data) => console.log(data));
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://www.freecodecamp.dev/curriculum-data/v1/available-superblocks.json', true)
 
+request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach(superblocks => {
+            console.log(superblocks.title)
+        })
+    } else {
+        console.log('error')
+    }
+}
+
+// Send request
+request.send()

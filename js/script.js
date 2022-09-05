73b17bd03c36468b1fe8cedd1e51f09e5a98ef6a
Jahi Grant <jahigrant@googlemail.com>
2022-09-05 13:33:18 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -3,11 +3,3 @@ fetch('https://www.freecodecamp.dev/curriculum-data/v1/available-superblocks.jso
 .then((response) => response.json())
 .then((data) => console.log(data));
 
-const result = await fetch('https://www.freecodecamp.dev/curriculum-data/v1/available-superblocks.json', {
-  method: 'GET',
-  mode: 'no-cors',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ foo: 'bar' }),
-}).then((data) => console.log(data));

4ec0a5c16879b56ccf4acc399118ad8b2b27c374
Jahi Grant <jahigrant@googlemail.com>
2022-09-17 15:52:25 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ function tick() {
     const element = (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {random_item(researchTitles)}.</h2>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
       </div>
     );
     ReactDOM.render(

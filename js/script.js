05606ba9011032c18fa71dbaaeaaf173136e1c69
Jahi Grant <jahigrant@googlemail.com>
2022-09-17 15:35:47 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
+
+const quoteTitles = [
+    ["Quote 1", "Quote Author 1"],
+    ["Quote 2", "Quote Author 2"],
+    ["Quote 3", "Quote Author 3"],
+    ["Quote 4", "Quote Author 4"],
+    ["Quote 5", "Quote Author 5"],
+    ["Quote 6", "Quote Author 6"]
+  ];
+
+  const getRandomQuoteTitle = () => {
+    return quoteTitles[Math.floor(Math.random() * quoteTitles.length)];
+  };
+
 function tick() {
+    const [quoteTitle, setQuoteTitle] = useState(getRandomQuoteTitle());
+    const handleClick = () => {
+        // shuffle array and pick random
+        const randomQuoteTitle = getRandomQuoteTitle();
+        setQuoteTitle(randomQuoteTitle);
+      };
+
     const element = (
       <div>
         <h1>Hello, world!</h1>
         <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        <h1>{quoteTitle}</h1>
+        <button onClick={handleClick}>Generate</button>
       </div>
     );
     ReactDOM.render(

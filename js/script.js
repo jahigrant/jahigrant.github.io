8c365876cff3531c7e5a333519a1323a7032f82b
Jahi Grant <jahigrant@googlemail.com>
2022-09-18 13:23:31 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -14,35 +14,46 @@
     return items.sort(() => Math.random() - 0.5).slice(0, 5); 
   }
 
+  function loadQuote() {
+    let randomItem = random_item(researchTitles);
+    let randomResult = [randomItem];
+    let quoteText = document.getElementById('quote');
+    let authorName = document.getElementById('author');
+    let myInsertText = randomResult[0][0][0];    
+    quoteText.innerHTML = myInsertText;
+    let myInsertAuthor = randomResult[0][0][1];
+    authorName.innerHTML = myInsertAuthor;
+  }
+
   function getQuote() {
     
     let randomItem = random_item(researchTitles);
-    let randomResult = [randomItem]
+    let randomResult = [randomItem];
 
     let btn = document.getElementById('theButton');
     let quoteText = document.getElementById('quote');
     let authorName = document.getElementById('author');
 
- 
-  btn.onclick = () => {
-    let myInsertText = randomResult[0][2][0];    
-    if (myInsertText !== quoteText.innerHTML) {
-      quoteText.innerHTML = myInsertText;
-      console.log(randomResult[0][2][0]);
-    } else {
-      quoteText.innerHTML = randomResult[0][0][0];
-      console.log(randomResult[0][0][0]);
-    }
+    btn.onclick = () => {
+      let myInsertText = randomResult[0][2][0];    
+      if (myInsertText !== quoteText.innerHTML) {
+        quoteText.innerHTML = myInsertText;
+        console.log(randomResult[0][2][0]);
+      } else {
+        quoteText.innerHTML = randomResult[0][0][0];
+        console.log(randomResult[0][0][0]);
+      }
 
-    let myInsertAuthor = randomResult[0][2][1];    
-    if (myInsertAuthor !== authorName.innerHTML) {
-      authorName.innerHTML = myInsertAuthor;
-      console.log(randomResult[0][2][1]);
-    } else {
-      authorName.innerHTML = randomResult[0][0][1];
-      console.log(randomResult[0][0][1]);
-    }
-  };
+      let myInsertAuthor = randomResult[0][2][1];    
+      if (myInsertAuthor !== authorName.innerHTML) {
+        authorName.innerHTML = myInsertAuthor;
+        console.log(randomResult[0][2][1]);
+      } else {
+        authorName.innerHTML = randomResult[0][0][1];
+        console.log(randomResult[0][0][1]);
+      }
+    };
+
   }
   
 
@@ -53,7 +64,7 @@ function tick() {
 
       <div>
         <button id='theButton' className="btn btn-primary rounded-0" onClick={getQuote}>New Quote</button>
-        <p className="d-block" id="quote" onLoad={getQuote}>Hello</p>
+        <p className="d-block" id="quote" onLoad={loadQuote}>Hello</p>
         <footer className="blockquote-footer d-block" id="author" >Bobby D</footer>
         <cite title="Source Title"></cite>
       </div>

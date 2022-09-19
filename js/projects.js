f76719091d4561de172a5fa82ecaae88d729be94
Jahi Grant <jahigrant@googlemail.com>
2022-09-19 08:43:21 +0100

    Initial commit

--- js/projects.js
+++ js/projects.js
@@ -393,7 +393,7 @@
     let randomItem = random_item(quoteItems);
     let randomResult = randomItem;
 
-    let quoteBox = document.getElementById('quote-box');
+    let tweetbtn = document.getElementById('tweet-quote');
     let quoteText = document.getElementById('text');
     let authorName = document.getElementById('author');
 
@@ -404,6 +404,8 @@
     
     let insertAuthor = randomResult[0].author;
     authorName.innerHTML = insertAuthor;
+    tweetbtn.href = "https://twitter.com/intent/tweet?text=" + insertQuote + insertAuthor;
+
   }
   
   window.addEventListener('load', function() {
@@ -425,8 +427,17 @@
     
 
     let btn = document.getElementById('new-quote');
+    let tweetbtn = document.getElementById('tweet-quote');
     let quoteText = document.getElementById('text');
     let authorName = document.getElementById('author');
+
+    // tweetbtn.onclick = () => {
+    //     let insertQuote = randomResult[0].quote;    
+
+    //       quoteText.innerHTML = insertQuote;
+    //       console.log(randomResult[0].quote);
+
+    // };
 
     btn.onclick = () => {
       let insertQuote = randomResult[0].quote;    
@@ -441,14 +452,18 @@
       let insertAuthor = randomResult[0].author;    
       if (insertAuthor !== authorName.innerHTML) {
         authorName.innerHTML = insertAuthor;
+        tweetbtn.href = "https://twitter.com/intent/tweet?text=" + insertQuote + insertAuthor;
         console.log(randomResult[0].author);
       } else {
         authorName.innerHTML = randomResult[1].author;
+        tweetbtn.href = "https://twitter.com/intent/tweet?text=" + randomResult[1].quote + randomResult[1].author;
         console.log(randomResult[1].author);
       }
     };
 
   }
+
+  tweetQuote
   
 
 
@@ -472,13 +487,15 @@ function tick() {
 
         <div className="row p-0 m-0">
           <div className="d-flex p-0 m-0">
-            <button id='tweet-quote' className="btn btn-primary rounded-1 text-start me-auto" onClick={getQuote}>
+            
+            <a id='tweet-quote' className="btn btn-primary rounded-1 text-start me-auto" href="#" role="button" onClick={tweetQuote}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter" viewBox="0 0 16 16">
                 <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
               </svg>
-            </button>
+            </a>
 
             <button id='new-quote' type="button" className="btn btn-primary rounded-1 text-end" onClick={getQuote}>New Quote</button>
+
           </div>
         </div>
         

d9ea19035756bc6f73c2af8376bce770dd648681
Jahi Grant <jahigrant@googlemail.com>
2022-09-20 13:48:28 +0100

    Initial commit

--- js/projects.js
+++ js/projects.js
@@ -392,7 +392,7 @@ const quoteItems = [
     return items.sort(() => Math.random() - 0.5).slice(0, 5); 
   }
 
-console.log(JSON.stringify(apiResponse, null, 2));
+console.log(JSON.stringify(apiResponse));
 
   function loadQuote() {
     let randomItem = random_item(quoteItems);

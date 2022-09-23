afcfd3cbc72bb44cd9dc605f7b31c37c5f4ed4a9
Jahi Grant <jahigrant@googlemail.com>
2022-09-23 13:36:40 +0100

    Initial commit

---
+++ js/projects-nav.js
@@ -0,0 +1,36 @@
+const navItems = [
+    {
+      "slug": "quote-machine",
+      "title": "Quote Machine",
+      "description": "A random quote machine",
+      "category": [
+        "react",
+        "html",
+        "javascript"
+      ]
+    }
+  ]; 
+
+function tick() {
+    const element = (
+
+      <ul className="nav nav-pills">
+      <li className="nav-item">
+        <a className="nav-link active" aria-current="page" href="#">Quotes</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+    </ul>
+
+    );
+    ReactDOM.render(
+      element,
+      document.getElementById('projects_nav')
+    );
+  }
+  
+tick();

892843ebcfc30b353ef55be3db1226fe4da6107e
Jahi Grant <jahigrant@googlemail.com>
2022-09-23 15:51:57 +0100

    Initial commit

--- js/projects-nav.js
+++ js/projects-nav.js
@@ -14,45 +14,15 @@ const navItems = [
 
   function navigation() {
     
-    window.addEventListener("load", function() {
-      // store tabs variable
-      var theTabs = document.querySelectorAll("ul.exeDIRECTtabsDIV-nav > li");
-      for (var i = 0; i < theTabs.length; i++) {
-      theTabs[i].addEventListener("click", theTabClicks)
-      }
-      function theTabClicks(tabClickEvent) {
-      var clickedTab = tabClickEvent.currentTarget;
-      var tabParent = tabClickEvent.currentTarget.parentNode.parentNode;
-      var theTabs = tabParent.querySelectorAll("ul.exeDIRECTtabsDIV-nav > li");
-      for (var i = 0; i < theTabs.length; i++) {
-      theTabs[i].classList.remove("active");
-      }
-      clickedTab.classList.add("active");
-      
-      //tabClickEvent.preventDefault(); //Stop Adding #ID In Addressbar
-      var contentPanes = tabParent.querySelectorAll(".exeDIRECTtabsDIV-pane");
-      for (var i = 0; i < contentPanes.length; i++) {
-      contentPanes[i].classList.remove("active");
-      }
-      var anchorReference = tabClickEvent.target;
-      var activePaneId = anchorReference.getAttribute("href");
-      var activePane = tabParent.querySelector(activePaneId);
-      activePane.classList.add("active");
-      }
-      // Open Address Bar Anchor Tag Tab Directly
-      var hash = window.location.hash; //get the hash from url
-      var cleanhash = hash.replace("#", ""); //remove the #
-      cleanhash = cleanhash.replace("-", "");
-      document.getElementById(cleanhash).click();
-      });
+    window.addEventListener("load", function() {});
           
   }
 
 function projectsNav() {
     const element = (
 
-      <ul className="exeDIRECTtabsDIV-nav nav nav-pills">
-        <li className="active nav-item">
+      <ul className="nav nav-pills">
+        <li className="nav-item">
           <a href="#tab-1" className="nav-link" id="tab1">Quote Machine</a>
         </li>
         <li className="nav-item">

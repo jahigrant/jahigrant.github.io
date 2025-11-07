04f317a531448580877f722c8d39ecb207801d73
Jahi Grant <jahigrant@googlemail.com>
2025-11-07 16:28:25 +0000

    Describe your changes

--- jahigrant/urls.py
+++ jahigrant/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for frontend project.
+URL configuration for jahigrant project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- jahigrant/wsgi.py
+++ jahigrant/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for frontend project.
+WSGI config for jahigrant project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frontend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jahigrant.settings')
 
 application = get_wsgi_application()
